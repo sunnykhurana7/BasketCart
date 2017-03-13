2fe9657c5126f3e047cbfb153998aa31d7929c04
Sunny Khurana <sunny.khurana@tothenew.com>
2017-03-13 08:27:39 +0530

    done

--- js/main.js
+++ js/main.js
@@ -41,57 +41,59 @@ $(document).ready(function(){
   document.getElementsByClassName('totalcost')[0].innerHTML = totalcost;
 
 
-  // $(".costupdate").change(function(){
-  //
-  //   var price = $(this).closest('tr').find('.price').text();
-  //
-  //   // for qty
-  //   // alert($(this).closest('td').find(input[type="number"]).value);
-  //
-  //   // alert($(this).closest('tr').find('.counter').text());
-  //
-  //   var obj = $(this).closest(('.costupdate'));
-  //   var qty = obj[0].value;
-  //
-  //   var cost = price * qty;
-  //
-  //   // for cost
-  //   $(this).closest('tr').find('.cost').text(cost);
-  //
-  //   // loop for printing the total
-  //   var total = 0;
-  //   // for loop to sum the cost and find vat
-  //   $("tr.item").each(function(){
-  //
-  //     total+= parseInt($(this).find('.cost').text());
-  //
-  //   });
-  //
-  //   document.getElementsByClassName('subtotal')[0].innerHTML = total;
-  //   var vat = (total * 20)/100;
-  //
-  //   document.getElementsByClassName('vat')[0].innerHTML = vat;
-  //
-  //
-  //   var totalcost = vat + total;
-  //
-  //   document.getElementsByClassName('totalcost')[0].innerHTML = totalcost;
-  //
-  //
-  // })
-  //
-  //
+  $(".costupdate").change(function(){
+
+    var price = $(this).closest('tr').find('.price').text();
+
+
+    var obj = $(this).closest(('.costupdate'));
+    var qty = obj[0].value;
+
+    var cost = price * qty;
+
+    // for cost
+    $(this).closest('tr').find('.cost').text(cost);
+
+    // loop for printing the total
+    var total = 0;
+    // for loop to sum the cost and find vat
+    $("tr.item").each(function(){
+
+      total+= parseInt($(this).find('.cost').text());
+
+    });
+
+    document.getElementsByClassName('subtotal')[0].innerHTML = total;
+    var vat = (total * 20)/100;
+
+    document.getElementsByClassName('vat')[0].innerHTML = vat;
+
+
+    var totalcost = vat + total;
+
+    document.getElementsByClassName('totalcost')[0].innerHTML = totalcost;
+
+
+  })
+
 
 
 
   // function of counter increase
   $(".inc").click(function(){
-    var val = $(this).parent().prev().val();
+    var val = $(this).parent().parent().prev().val();
+
     var inc = parseInt(val) + 1;
 
-    $(this).parent().prev().val(inc);
+    $(this).parent().parent().prev().val(inc);
 
-    var co = $(this).parent().prev().val();
+    // conditions
+    if(val>=10){
+      $(this).parent().parent().siblings('.costupdate').val(10);
+    }
+
+
+    var co = $(this).parent().parent().prev().val();
 
     var price = $(this).closest('tr').find('.price').text();
 
@@ -125,20 +127,24 @@ $(document).ready(function(){
 
 // function for decrement
 $(".dec").click(function(){
-  var val = $(this).parent().siblings('.costupdate').val();
+  var val = $(this).parent().parent().siblings('.costupdate').val();
 
   var inc = parseInt(val) - 1;
 
-  $(this).parent().siblings('.costupdate').val(inc);
+  $(this).parent().parent().siblings('.costupdate').val(inc);
 
-  var co = $(this).parent().siblings('.costupdate').val();
+  // conditions
+  if(val<=1){
+    $(this).parent().parent().siblings('.costupdate').val(0);
+  }
+
+  var co = $(this).parent().parent().siblings('.costupdate').val();
 
   var price = $(this).closest('tr').find('.price').text();
 
   var total = co * price;
 
   $(this).closest('tr').find('.cost').text(total);
-
 
   // loop for printing the total
   var total = 0;
@@ -154,16 +160,9 @@ $(".dec").click(function(){
 
   document.getElementsByClassName('vat')[0].innerHTML = vat;
 
-
   var totalcost = vat + total;
 
   document.getElementsByClassName('totalcost')[0].innerHTML = totalcost;
 
-
 });
-
-
-
-
-
 });
